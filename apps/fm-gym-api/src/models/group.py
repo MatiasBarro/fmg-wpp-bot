c35c704241f1cb4009921c292fbd721b052f44a0
Matias Barro <matias.barro@gmail.com>
2025-08-06 17:34:02 -0300

    Add created_at and updated_at fields in groups tables

--- apps/fm-gym-api/src/models/group.py
+++ apps/fm-gym-api/src/models/group.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import datetime, time, timezone
 from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 
@@ -11,6 +11,8 @@ class GroupBase(SQLModel):
 class Group(GroupBase, table=True):
     id: int = Field(default=None, primary_key=True)
     schedule: List["GroupSchedule"] = Relationship(back_populates="group")
+    created_at: datetime = Field(default=datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default=datetime.now(timezone.utc), nullable=False)
 
 class GroupScheduleBase(SQLModel):
     day: int
@@ -21,3 +23,5 @@ class GroupSchedule(GroupScheduleBase, table=True):
     id: int = Field(default=None, primary_key=True)
     group_id: Optional[int] = Field(foreign_key="group.id")
     group: Optional[Group] = Relationship(back_populates="schedule")
+    created_at: datetime = Field(default=datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default=datetime.now(timezone.utc), nullable=False)
